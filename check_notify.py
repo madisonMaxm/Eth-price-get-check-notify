ded5f99dc9874f6b8a0b1f98f8861fb5c9b4f07f
Max Meller <mmeller@wisc.edu>
2017-07-08 22:30:25 -0500

    removed testing features

--- check_notify.py
+++ check_notify.py
@@ -10,15 +10,15 @@ search_url = 'https://coinmarketcap.com/currencies/ethereum/'
 content = urllib2.urlopen(search_url).read()
 soup = BeautifulSoup(content, "lxml")
 
-price = soup.find(id = 'quote_price').getText()
-ascii_price = (price.encode('ascii'))
+str_price = soup.find(id = 'quote_price').getText()
+ascii_price = (str_price.encode('ascii'))
 price = float(ascii_price[1:])
 
 def compare_price():
 	if price <= config.lower_limit or price >= config.upper_limit:
 		return true
 		
-#yagmail used because of attachment sending capabilities. Uses keyring for login
+#sends email from email in config file to same email
 def send_email(address, password):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -28,8 +28,6 @@ def send_email(address, password):
     msg = "The price of ETH is outside your limit $" + str(price) + "."
     server.sendmail(address, address, msg)
     server.quit()
-    
-send_email(config.email_address, config.password)
    
 if __name__ == '__main__':
     try:

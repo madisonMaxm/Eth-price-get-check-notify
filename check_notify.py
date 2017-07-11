ae714365a811056b7dd860b02131536547611568
Max Meller <mmeller@wisc.edu>
2017-07-10 21:40:06 -0500

    added time module and sleep method

--- check_notify.py
+++ check_notify.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
+import config
+import smtplib
+import time
+import urllib2
 
 from bs4 import BeautifulSoup
-import urllib2
-import smtplib
-import config
 
 # Coinmarket Cap ETH
 search_url = 'https://coinmarketcap.com/currencies/ethereum/'
 content = urllib2.urlopen(search_url).read()
 soup = BeautifulSoup(content, "lxml")
 
-str_price = soup.find(id = 'quote_price').getText()
-ascii_price = (str_price.encode('ascii'))
-price = float(ascii_price[1:])
+run = True
 
-def compare_price():
-	if price <= config.lower_limit or price >= config.upper_limit:
-		return True
+#returns price as float
+def parse_price():
+    str_price = soup.find(id = 'quote_price').getText()
+    ascii_price = (str_price.encode('ascii'))
+    price = float(ascii_price[1:])
+    return price
+
+def compare_price(value):
+    if value <= config.lower_limit or value >= config.upper_limit:
+        return True
 		
 #sends email from email in config file to same email
 def send_email(address, password):
@@ -30,10 +36,14 @@ def send_email(address, password):
     server.quit()
    
 if __name__ == '__main__':
-    try:
-        parse_price()
-        if compare_price():
-            send_email(config.email_address, config.password)
+    
+    while run:
+        price = parse_price()
+        try:
+            if compare_price(price):
+                send_email(config.email_address, config.password)
+                time.sleep(1800) #code only runs once every half an hour
 
-    except:
-		pass
+        except:
+            print("Error. Program exiting")
+            run = False
